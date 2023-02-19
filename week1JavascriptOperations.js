a64f121ef6c8030825e41f5aa374628b31f5d768
SAEED <123606986+saeedna1@users.noreply.github.com>
2023-02-19 15:34:45 -0500

    Add files via upload

---
+++ week1JavascriptOperations.js
@@ -0,0 +1,107 @@
+//Week 1: OpenClass - Javascript Operations
+
+
+//Boolean Expression using Greater Than or Equal To:
+//Using console.log(), print num1 is greater than or equal to num2.
+console.log(num1 >= num2);
+//waht is the value of x?
+var num1 = 23;
+var num2 = 23;
+var x = num1 < num2;
+console.log (x);
+
+
+
+//Addition & Multiplication:Using 
+//console.log(), printthe final result of the following:calculate the product of num1 and num2, and add 10 to that product.
+console.log((num1 * num2) + 10);
+// what is the value of z?
+var x = 5;
+var y = 6;
+x = 3 +y
+var z = x + y;
+y = 5 + 2*x
+x=y
+console.log (z);
+
+
+
+//Subtraction & Multiplication:
+//using console.log(), print the final result of the following:subtract num2 from num1 and multiply that result by 16.
+console.log((num1 - num2)*16);
+// waht is the value of Z?
+var x = 8 ;
+y = 12 ;
+x = y + x ;
+z = x + y ;
+console.log (z);
+
+
+
+//The == comparison operator compares values while the === comparison operator compares values and data types.
+console.log (num1 == num2);
+
+
+
+//Subtraction:
+//Using console.log(), given the variables num1 and num2,
+console.log(num2 - num1);
+//What is the value of z at the end of this code block? print the result of subtracting num1 from num2.
+var x = 35;
+var y = "20";
+var z = x + y;
+console.log(z)
+
+
+
+//Addition:
+//Using console.log(), given variables num1 and num2,print the result of adding num1 and num2.
+console.log(num1 + num2);
+//What is the final value of x at the end of this code block?
+var x = 45;
+var y = 16;
+x = x - y;
+console.log(x);
+
+
+
+//Boolean Expression using a Comparison Operator:
+//Using console.log(), print the following: num1 is less than num2.
+console.log(num1 < num2);
+//What is the value of x?
+var num1 = 3;
+var num2 = 2
+var x = num1 * 2 + num2 * 3
+console.log(x);
+
+
+
+//Addition & Division:
+//Using console.log(), print the final result of the following: calculate the sum of num1 and num2, divided by 5.
+console.log((num1 + num2)/5);
+//What is the value of z?
+var x = 25;
+var y = 3;
+var z = "Z = " + (x*2);
+console.log (z);
+
+
+
+//Division:
+//Using console.log(), given the variables num1 and num2,print the quotient of num1 over num2.
+console.log(num1 / num2);
+//What is the value of z at the end of this code block?
+var x;
+var y = 38;
+var z = x + y;
+console.log(z);
+
+
+
+//Multiplication:
+//Using console.log(), given the variables num1 and num2,print the product of num1 and num2.
+console.log(num1 * num2);
+//What is the final value of x at the end of this code block?
+var x = 30;
+var y = 6;
+x = x / y;
---
+++ week1JavascriptVariables.js
@@ -0,0 +1,108 @@
+//Week 1: OpenClass - Javascript Variables
+
+
+//Decimal Variable:
+//Create a variable called itemPrice and set it equal to the price of your favorite item on Amazon.
+//Be sure to include the decimal point and the cents.
+var itemPrice = 100.00;
+console.log(itemPrice);
+//What is the data type of the following variable? A-String
+var number = "42"
+console.log(String);
+
+
+
+//Integer variable:
+//Create a variable called age and set it equal to 25.
+var age = 25;
+console.log(age);
+//what is the data type of x in the following code? A- String
+var x = "number";
+console.log(String);
+
+
+
+//Name Variable:
+//Declare a variable called fullName and set the value to 'Johnny Appleseed'
+var fullName = 'Johnny Appleseed';
+console.log(fullName);
+//Which of the following is not a recognized data type in JavaScript
+//answer is Character
+
+
+
+
+//Hungry for Italian Food?
+//Declare a variable called isHungry and assign the value true
+//Then, declare a second variable called craving and assign the value 'italian food'
+var isHungry = true;
+var craving = "italian food";
+//What is the value of x? A-undefined
+var x;
+console.log(undefined);
+
+
+
+
+//Favorite Game:
+//Reassign the given variable favoriteGame to another game of your choice.
+var favoriteGame = 'Soccer';
+console.log (favoriteGame);
+//What is the data type of the following variable? A- Null
+var firstName = null;
+
+
+
+
+//Is My Computer On?
+//Declare a variable called computerIsOn and set it equal to true.
+var computerIsOn = true;
+//What is the data type of the following variable? A- Boolean
+var string = true;
+console.log(computerIsOn);
+
+
+
+
+//Boolean variable:
+//Create a variable called isHotOutside and set it equal to an appropriate value.
+var isHotOutside = true;
+console.log(isHotOutside);
+//What is the data type of x? A-Strling
+var x = "800";
+console.log(isHotOutside);
+
+
+
+
+//What is your Lucky Number?
+//Declare a variable called luckyNumber and set it equal to your lucky number.
+let luckyNumber = 100;
+console.log(luckyNumber);
+//In JavaScript, you can use either single quotes (' ') or double quotes (" ") to create a string but you must use the same quote type at the beginning and end of each string.
+//the answer is A- True
+
+
+
+
+//What is the current year?
+//Reassign the given variable currentYear to the current year.
+var currentYear =2023;
+console.log(currentYear);
+//What is the data type of x in the following statement? A- Boolean
+var x = true;
+
+
+
+//Create the full name from a first and a last name:
+//Declare three variables: firstName, lastName, and fullName.
+//Set the firstName and lastName variables to your first and last name, respectively.
+//Then, set the fullName variable equal to the concatenation of the two variables with a space in between.
+var firstName = 'saeed';
+var lastName = 'adem';
+var fullName = firstName + " " + lastName
+console.log(fullName);
+
+//What is the data type of the following variable? A-Number
+var string = 32;
+console.log(32);
